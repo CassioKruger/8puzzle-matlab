465018dc86d4ad66b90e9f4cb3323b5f956a7f3b
CassioKruger <krugercassio@gmail.com>
2019-05-08 01:51:56 -0300

    novos movimentos e geração das estruturas

--- BFS.m
+++ BFS.m
@@ -1,6 +1,9 @@
 function [passos,iteracoes,inversoes,flag_solucao,estado_atual] = BFS(estado_inicial,estado_meta)
 
-    %inicializando variaveis
+    % inicializando variaveis
+    atual = 1;              % indice para as estruturas
+    flag_terminou = 0;      % flag para indicar se o estado meta foi aberto
+     
     passos = 0;
     iteracoes = 0;
     inversoes = 0;
@@ -11,18 +14,18 @@ function [passos,iteracoes,inversoes,flag_solucao,estado_atual] = BFS(estado_ini
 
     %% ---------------------------%%
 
-    start = [estado_inicial(1:3);estado_inicial(4:6);estado_inicial(7:9)];  %converte a array do estado_inicial para uma matriz
+    start = [estado_inicial(1:3);estado_inicial(4:6);estado_inicial(7:9)];  % converte a array do estado_inicial para uma matriz
     goal = [estado_meta(1:3);estado_meta(4:6);estado_meta(7:9)];
     
-    estado_atual = sprintf('%d', estado_inicial);   %para printar    
+    estado_atual = sprintf('%d', estado_inicial);   % para printar    
     
-    start
-    goal
+    grafo.fronteira(atual) = {start};           % add estado inicial ao grafo (primeira camada)
+    grafo.fronteira{atual}  
     
-    %verificacao se para o estado inicial, existe solucao
-    %se o numero de inversoes for PAR, existe solucao
-    %se for IMPAR, nao existe
-
+    grafo.meta(1) = {goal};                     % add estado meta ao grafo
+    grafo.meta{:}
+    
+    % verificacao se para o estado inicial, existe solucao -> IMPAR: sem solucao; PAR: com solucao
     for col = 1:8
         for aux = (col+1):9
             if(estado_inicial(col) > estado_inicial(aux)) && (estado_inicial(aux)>0)
@@ -31,228 +34,88 @@ function [passos,iteracoes,inversoes,flag_solucao,estado_atual] = BFS(estado_ini
         end
     end
 
-    if (mod(inversoes,2) == 0)        %testa se o RESTO DE 2 de inversoes eh igual a zero
-        flag_solucao = 1;
+    if (mod(inversoes,2) == 0)        % testa se o RESTO DE 2 de inversoes eh igual a zero
+        flag_solucao = 1;             % existe solucao
     else
-        flag_solucao = 0;
+        flag_solucao = 0;             % n existe solucao
     end
 
     %% -------------------------%%
     %%  comeco do algoritmo BFS %%
     %% -------------------------%%
-
+       
     % fazer uma queue/consulta
     if (flag_solucao == 1)
+        % os proximos movimentos dependem de onde o ZERO esta;
+        % se estiver nos cantos, possui mais 2 movimentos possiveis;
+        % se estiver nas laterais, mas no quadrado do meio, possui mais 3 movimentos possiveis;
+        % se estiver no meio, possui mais 4 movimentos possiveis;
 
-        A{1,1} = start;
-        A{2,1} = 0;
-        CloseList{1,1}= A{1,1}; %Lista fechada
-        CloseList{2,1}= 1;
-        CloseList{3,1}= 0;
-        OpenList={[]};
-        OpenList{2,1}=1;
-        least_open=1;
-        k=1;
-        i=1;
-        k=2;
-        father=1;
-        var=0;
-        N=0;
+        [lin, col] = find( grafo.fronteira{atual} == 0);        %procura a posicao do zero na matriz e retorna lin e col;
 
-        while var==0
-            [lixo,least_close]=size(CloseList);
-            %infos sobre a possibilidade de movimento do espaco em vazio;
-            [row,colunm]=find(A{1,1}==0);%informa a localizacao do zero
-
-            left=colunm-1;
-            right=colunm+1;
-            up=row-1;
-            down=row+1;
-
-            %Movimento para esquerda
-            if(left>=1)
-                NumberLeft=A{1,1}(row,left);%Salva o valor a esquerda do espa�o em vazio
-                B=A{1,1};%Salva a matriz a em uma vari�vel tempor�ria
-                B(row,left)=0;%Desloca o espa�o vazio para esquerda
-                B(row,colunm)=NumberLeft;%Desloca o valor da esquerda para a direita
-                %%pause(1);
-                for j=1:least_close
-                    if (((sum(sum(B~=CloseList{1,j})))==0))
-                        N=1;
-                    end
-                end
-
-                if (N==0)
-                    OpenList{1,least_open}=B;
-                    OpenList{1,least_open+1}=[];
-
-                    OpenList{2,least_open}=k;
-                    OpenList{3,least_open}=father;                             
-                    OpenList{4,least_open}=(A{2,1}+1);
-                    [lixo,least_open]=size(OpenList);
-                    k=k+1;
-                end
-                if (sum(sum(goal~=B)))==0           %se a soma de goal ~=B for zero, achou a solucao
-                    var=1;
-                    solution{1,1}= OpenList{1,least_open-1};
-                    solution{2,1}= OpenList{2,least_open-1};
-                    solution{3,1}= OpenList{3,least_open-1};
-                    solution{4,1}= OpenList{4,least_open-1};
-                end        
-            end   
-            N=0;
-
-            if((right<=3))
-                [lixo,least_open]=size(OpenList);
-                [lixo,least_open]=size(OpenList);
-                NumberRight=A{1,1}(row,right);%Salva o valor a direita do espa�o em vazio
-                B=A{1,1};%Salva a matriz a em uma vari�vel tempor�ria
-                B(row,right)=0;%Desloca o espa�o vazio para esquerda
-                B(row,colunm)=NumberRight;%Desloca o valor da esquerda para a direita
-                %pause(1);
-                for j=1:least_close
-                    if (((sum(sum(B~=CloseList{1,j})))==0))
-                        N=1;
-                    end
-                end
-
-                if (N==0)
-                    OpenList{1,least_open}=B;
-                    OpenList{1,least_open+1}=[];
-
-                    OpenList{2,least_open}=k;
-                    OpenList{3,least_open}=father;
-                    OpenList{4,least_open}=(A{2,1}+1);
-                    [lixo,least_open]=size(OpenList);    
-                    k=k+1;
-                end
-
-                if (sum(sum(goal~=B)))==0
-                    var=1;
-                    solution{1,1}= OpenList{1,least_open-1};
-                    solution{2,1}= OpenList{2,least_open-1};
-                    solution{3,1}= OpenList{3,least_open-1};
-                    solution{4,1}= OpenList{4,least_open-1};
-                end
-            end 
-
-
-            N=0;
-            if(up>=1)
-                NumberUp=A{1,1}(up,colunm);%Salva o valor acima do espa�o em vazio;
-                B=A{1,1};%Salva a matriz a em uma vari�vel tempor�ria
-                B(up,colunm)=0;%Desloca o espa�o vazio para esquerda
-                B(row,colunm)=NumberUp;%Desloca o valor da esquerda para a direita
-                %pause(1);
-                for j=1:least_close
-                    if (((sum(sum(B~=CloseList{1,j})))==0))
-                        N=1;
-                    end
-                end
-
-                if (N==0)
-                    OpenList{1,least_open}=B;
-                    OpenList{1,least_open+1}=[];
-
-                    OpenList{2,least_open}=k;
-                    OpenList{3,least_open}=father;
-                    OpenList{4,least_open}=(A{2,1}+1);
-                    [lixo,least_open]=size(OpenList);
-                    k=k+1;
-                end
-
-                if (sum(sum(goal~=B)))==0
-                    var=1;
-                    solution{1,1}= OpenList{1,least_open};
-                    solution{2,1}= OpenList{2,least_open-1};
-                    solution{3,1}= OpenList{3,least_open-1};
-                    solution{4,1}= OpenList{4,least_open-1};
-                end
-            end
-
-            N=0;
-            if((down<=3))
-                NumberDown=A{1,1}(down,colunm);%Salva o valor acima do espa�o em vazio;
-                B=A{1,1};%Salva a matriz a em uma vari�vel tempor�ria
-                B(down,colunm)=0;%Desloca o espa�o vazio para esquerda
-                B(row,colunm)=NumberDown;%Desloca o valor da esquerda para a direita
-                %pause(1);
-                for j=1:least_close
-                    if (((sum(sum(B~=CloseList{1,j})))==0))
-                        N=1;
-                    end
-                end
-
-                if (N==0)
-                    OpenList{1,least_open}=B;
-                    OpenList{1,least_open+1}=[];
-
-                    OpenList{2,least_open}=k;
-                    OpenList{3,least_open}=father;
-
-                    OpenList{4,least_open}=(A{2,1}+1);
-                    [lixo,least_open]=size(OpenList);
-                    k=k+1;
-                end
-
-                if (sum(sum(goal~=B)))==0
-                    var=1;
-                    solution{1,1}= OpenList{1,least_open-1};
-                    solution{2,1}= OpenList{2,least_open-1};
-                    solution{3,1}= OpenList{3,least_open-1};
-                    solution{4,1}= OpenList{4,least_open-1};
-                end
-            end 
+        % movimentacoes
+        if(lin < 3)
+            auxElemento = grafo.fronteira{atual}(lin+1, col);   % salva o valor que esta em cima do vazio
+            auxMatriz = grafo.fronteira{atual};
+            auxMatriz(lin+1, col) = 0;                          % move o espaco vazio
+            auxMatriz(lin, col) = auxElemento;                  % move o elemento para o espaco vazio anterior
             
-            N=0;
-            [lixo,least_close]=size(CloseList);
-            CloseList{1,least_close+1}= OpenList{1,1};
-            CloseList{2,least_close+1}= OpenList{2,1};
-            CloseList{3,least_close+1}= OpenList{3,1};
-            CloseList{4,least_close+1}= OpenList{4,1};
-
-            OpenList(:,1)=([]);
-
-            [lixo,least_open]=size(OpenList);
-            [lixo,least_close]=size(CloseList);
-
-            father=CloseList{2,least_close};
-
-            A{1,1}=CloseList{1,least_close};
-            A{2,1}=CloseList{4,least_close};
-
-        end %%%
-
-        CloseList=cat(2,CloseList,OpenList(1:4,1:least_open-1));
-        map{1,1}= solution{1,1};
-        map{2,1}= solution{2,1};
-        map{3,1}= solution{3,1};
-        map{4,1}= solution{4,1};
-        way=solution{3,1};
-        CloseList=cat(2,CloseList,OpenList);
-        [lixo,least_close]=size(CloseList);
-        %
-        for k=least_close:-1:1
-            if CloseList{2,k}==way
-                i=i+1;
-                map{1,i}= CloseList{1,k};
-                map{2,i}= CloseList{2,k};
-                map{3,i}= CloseList{3,k};
-                map{4,i}= CloseList{4,k};
-
-                way=map{3,i};
+            % verifica se o novo estado já foi visitado antes; caso sim, nao add na fronteira
+            flagJaAberto = find(arrayfun(@(s) (isequal(auxMatriz, s.fronteira)==1), grafo));
+            if flagJaAberto == 0
+                grafo.fronteira(end+1) = {auxMatriz};           % add o novo estado na fronteira
             end
         end
-
-        iteracoes = i;
-
-        while i~=0
-            passos{1,i} = map{1,i};
-            i=i-1;
+    
+        if(lin > 1)
+            auxElemento = grafo.fronteira{atual}(lin-1, col);   % salva o valor que esta embaixo do vazio
+            auxMatriz = grafo.fronteira{atual};
+            auxMatriz(lin-1, col) = 0;                          % move o espaco vazio
+            auxMatriz(lin, col) = auxElemento;                  % move o elemento para o espaco vazio anterior
+            grafo.fronteira(end+1) = {auxMatriz};               % add o novo estado na fronteira
+    
+            % verifica se o novo estado já foi visitado antes; caso sim, nao add na fronteira
+            flagJaAberto = find(arrayfun(@(s) (isequal(auxMatriz, s.fronteira)==1), grafo));
+            if flagJaAberto == 0
+                grafo.fronteira(end+1) = {auxMatriz};           % add o novo estado na fronteira
+            end
         end
-
-        passos
-        iteracoes
+    
+        if(col > 1)    
+            auxElemento = grafo.fronteira{atual}(lin, col-1);  % salva o valor que esta na esquerda do vazio
+            auxMatriz = grafo.fronteira{atual};
+            auxMatriz(lin, col-1) = 0;                          % move o espaco vazio
+            auxMatriz(lin, col) = auxElemento;                  % move o elemento para o espaco vazio anterior
+            grafo.fronteira(end+1) = {auxMatriz};              % add o novo estado na fronteira
+    
+            % verifica se o novo estado já foi visitado antes; caso sim, nao add na fronteira
+            flagJaAberto = find(arrayfun(@(s) (isequal(auxMatriz, s.fronteira)==1), grafo));
+            if flagJaAberto == 0
+                grafo.fronteira(end+1) = {auxMatriz};           % add o novo estado na fronteira
+            end
+        end
+        
+        if(col < 3)
+            auxElemento = grafo.fronteira{atual}(lin, col+1);  % salva o valor que esta na direita do vazio   
+            auxMatriz = grafo.fronteira{atual};
+            auxMatriz(lin, col+1) = 0;                         % move o espaco vazio
+            auxMatriz(lin,col) = auxElemento;                  % move o elemento para o espaco vazio anterior
+            grafo.fronteira(end+1) = {auxMatriz};              % add o novo estado na fronteira
+    
+            % verifica se o novo estado já foi visitado antes; caso sim, nao add na fronteira
+            flagJaAberto = find(arrayfun(@(s) (isequal(auxMatriz, s.fronteira)==1), grafo));
+            if flagJaAberto == 0
+                grafo.fronteira(end+1) = {auxMatriz};           % add o novo estado na fronteira
+            end
+        end
+    
+        grafo.visitados(aberto) = grafo.fronteira(atual);      % salva o estado atual nos visitados
+        grafo.fronteira(atual) = [];                           % retira o estado recem aberto da fronteira
+    
+        aberto = aberto + 1;
+        %atual = atual + 1;
+        
     end
+        
 
 end
